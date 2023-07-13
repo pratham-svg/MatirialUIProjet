b09e6fa6ad9dcbc0122711277e7548df55421e6b
aryan8sirohi <aryanchauhan@microlent.com>
2023-07-13 17:15:18 +0530

    calculator, contact pages added to mechanicalCal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css'
 import Footer from './components/common/Footer';
 import Header from './components/common/Header';
+import Contact from './components/Pages/Contact';
+import Calculator from './components/Pages/Calculator';
 
 
 function App() {
   return (
     <div >
    <Header/>
- 
+   <Contact/>
+   {/* <Calculator/> */}
    <Footer/>
     </div>
   );
