e94f377afdd7ed5d7f274848022a7e2d59cc0f15
BhatiMohit11 <mohit.bhati@microlent.com>
2023-07-13 17:17:14 +0530

    pages of pricing and pricingList

---
+++ src/components/Pages/PricingList.js
@@ -0,0 +1,134 @@
+import React from 'react'
+
+const PricingList = () => {
+  return (
+    <div>  <section className="container-fluid Project-section">
+    <div className="container">
+        <div className="row mb-5 ">
+            <div className="col-md-12 mt-4">
+                <div className="d-flex flex-wrap align-items-center justify-content-between">
+                    <h3 className="fw-bold me-3">Projects List</h3>
+                    <button className="btn btn-New-project"><i className="fa-solid fa-plus me-1"></i> New Project</button>
+                </div>
+
+            </div>
+            <div className="col-md-12 mt-4">
+                <div className="card border-0">
+                    <div className="card-body p-0">
+                        <div className="table-responsive">
+                            <table className="table table-bordered">
+                                <thead>
+                                    <tr className="table-header">
+                                        <th>S. No.</th>
+                                        <th>Project Name</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            01
+                                        </td>
+                                        <td>
+                                            Abc
+                                        </td>
+                                        <td>
+                                            <div className="table-action-icon">
+                                                <a href=""><i className="fa-regular fa-eye"></i></a>
+                                                <a href=""><i className="fa-regular fa-pen-to-square"></i></a>
+                                                <a href=""><i className="fa-regular fa-trash-can"></i></a>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            02
+                                        </td>
+                                        <td>
+                                            Abc
+                                        </td>
+                                        <td>
+                                            <div className="table-action-icon">
+                                                <a href=""><i className="fa-regular fa-eye"></i></a>
+                                                <a href=""><i className="fa-regular fa-pen-to-square"></i></a>
+                                                <a href=""><i className="fa-regular fa-trash-can"></i></a>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            03
+                                        </td>
+                                        <td>
+                                            Abc
+                                        </td>
+                                        <td>
+                                            <div className="table-action-icon">
+                                                <a href=""><i className="fa-regular fa-eye"></i></a>
+                                                <a href=""><i className="fa-regular fa-pen-to-square"></i></a>
+                                                <a href=""><i className="fa-regular fa-trash-can"></i></a>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            04
+                                        </td>
+                                        <td>
+                                            Abc
+                                        </td>
+                                        <td>
+                                            <div className="table-action-icon">
+                                                <a href=""><i className="fa-regular fa-eye"></i></a>
+                                                <a href=""><i className="fa-regular fa-pen-to-square"></i></a>
+                                                <a href=""><i className="fa-regular fa-trash-can"></i></a>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            05
+                                        </td>
+                                        <td>
+                                            Abc
+                                        </td>
+                                        <td>
+                                            <div className="table-action-icon">
+                                                <a href=""><i className="fa-regular fa-eye"></i></a>
+                                                <a href=""><i className="fa-regular fa-pen-to-square"></i></a>
+                                                <a href=""><i className="fa-regular fa-trash-can"></i></a>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            06
+                                        </td>
+                                        <td className="w-50">
+                                            Abc
+                                        </td>
+                                        <td>
+                                            <div className="table-action-icon">
+                                                <a href=""><i className="fa-regular fa-eye"></i></a>
+                                                <a href=""><i className="fa-regular fa-pen-to-square"></i></a>
+                                                <a href=""><i className="fa-regular fa-trash-can"></i></a>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+</div>
+
+</section>
+</div>
+  )
+}
+
+export default PricingList
