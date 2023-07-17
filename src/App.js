c4359b6416b08f7f45f744015ef75db1d580169e
BhatiMohit11 <mohit.bhati@microlent.com>
2023-07-17 11:05:21 +0530

    App created

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
 import './App.css'
-import Header from './components/common/Header';
-import Home from './components/Pages/Home'
-import { BrowserRouter as Router , Routes , Route } from 'react-router-dom';
-import About from './components/Pages/About';
-import Footer from './components/common/Footer';
-import Contact from './components/Pages/Contact'
-import Pricing from './components/Pages/Pricing';
-import Calculator from './components/Pages/Calculator';
-import Signup from './components/Pages/signup';
-import ForgotPassword from './components/Pages/ForgotPassword';
-import ResetPassword from './components/Pages/ResetPassword';
 
 function App() {
  
   return (
     <div>
-    <Router>
-    <Header/>
-    <Routes>
-    <Route path='/' element={<Home/>}/>
-    <Route path='/About' element={<About/>}/>
-    <Route path='/Calculator' element={<Calculator/>}/>
-    <Route path='/Contact' element={<Contact/>}/>
-    <Route path='/Pricing' element={<Pricing/>}/>
-    <Route path='/Signup' element={<Signup/>}/>
-    <Route path='/ForgotPassword' element={<ForgotPassword/>} />
-    <Route path='/ResetPassword' element={<ResetPassword/>} />
-    </Routes>
-    <Footer/>
-    </Router>
+     Home
     </div>
   );
 }
--- src/Redux/Reducer/index.js
+++
@@ -1,8 +0,0 @@
-import { createSlice , createAsyncThunk } from "@reduxjs/toolkit";
-
-const intialState = {
-    todo : [],
-    status : 'idle' ,
-    error : null
-}
-
